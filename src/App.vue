78e164385cc3e5a45d3f0eff87227f7be87b71b7
Samarreddy12 <101542906+Samarreddy12@users.noreply.github.com>
2023-04-27 09:09:32 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="app">
+    <Header />
+    <main class="main">
+      <JobList :jobs="jobs" />
+    </main>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from './components/HeaderComponent.vue'
+import JobList from './components/JobList.vue'
+import Footer from './components/FooterComponent.vue'
+
+export default {
+  components: {
+    Header,
+    JobList,
+    Footer
+  },
+  data() {
+    return {
+      jobs: []
+    }
+  },
+  async created() {
+    
+    const response = await fetch('https://jobhive-back.herokuapp.com/api/jobs')
+    const data = await response.json()
+    this.jobs = data
+  }
+}
+</script>
+
+<style>
+.app {
+  display: grid;
+  grid-template-rows: auto 1fr auto;
+  min-height: 100vh;
+  font-family: Arial, sans-serif;
+}
+
+.main {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  grid-gap: 20px;
+  padding: 20px;
+}
+</style>
---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,40 @@
+<template>
+  <footer class="footer">
+    <div class="container">
+      <p>&copy; {{ currentYear }} JobHive. All rights reserved.</p>
+    </div>
+  </footer>
+</template>
+
+  <script>
+  export default {
+    data() {
+      return {
+        currentYear: new Date().getFullYear()
+      }
+    }
+  }
+  </script>
+  
+  <style>
+  .footer {
+    display: grid;
+    grid-template-columns: 1fr;
+    background-color: #f5f5f5;
+    padding: 20px;
+    font-size: 14px;
+    text-align: center;
+    box-shadow: 0 -2px 4px rgba(0, 0, 0, 0.1);
+  }
+  
+  .container {
+    display: grid;
+    grid-template-columns: auto 1fr;
+    align-items: center;
+    max-width: 960px;
+    margin: 0 auto;
+  }
+  
+ 
+  </style>
+  
